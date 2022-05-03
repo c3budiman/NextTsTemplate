25ded5c1cfc2f979cc14211b896fa10edee4fe83
Cecep Budiman <c3budiman@gmail.com>
2022-05-03 10:27:43 +0700

    fixing codefactor issue

--- tests/pages/index.spec.tsx
+++ tests/pages/index.spec.tsx
@@ -4,8 +4,6 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import MatchMediaMock from "jest-matchmedia-mock";
 import HeaderOur from "../../Components/Global/Header";
-import MarketListing from "../../Components/Home/Table/MarketListing";
-import { TestingData } from "../../Configs/TestingData";
 
 let matchMedia: MatchMediaMock;
 
@@ -35,24 +33,5 @@ describe("Home", () => {
     );
     const headerUser = HeaderComp.container.querySelector("#headerPeopleName")?.innerHTML;
     expect(headerUser).toBe(name);
-  });
-
-  it("Renders the market table", async () => {
-    const marketComp = render(
-      <MarketListing
-        errorState={false}
-        loadingState={false}
-        readyState
-        data={TestingData}
-        setData={() => {}}
-      />
-    );
-    // marketComp.debug(undefined, 300000);
-    const test = await marketComp.getAllByTestId("assetName");
-    // test data has 2 data, one is btc, and one is ftm.
-    // so if it was rendered correctly, it shall output 2 cell column.
-    expect(test?.length > 0).toBe(true);
-    expect(test[0].innerHTML).toBe("BTC");
-    expect(test[1].innerHTML).toBe("Fantom");
   });
 });
